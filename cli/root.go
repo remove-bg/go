52e5b38f2fb98a7219d584278575f564f380c3a3
Oliver Peate <oliverp@gmail.com>
2019-04-11 11:06:48 +0100

    Set CLI metadata

--- cli/root.go
+++ cli/root.go
@@ -10,6 +10,10 @@ import (
 func Bootstrap() *cli.App {
 	app := cli.NewApp()
 
+	app.Name = "removebg"
+	app.Description = "Remove image background - 100% automatically"
+	app.Version = "0.1.0"
+
 	app.Action = func(c *cli.Context) error {
 		fmt.Println("Hello World")
 		return nil
